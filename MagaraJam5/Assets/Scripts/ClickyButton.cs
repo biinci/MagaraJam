c2fa9e82f9a39ebca9036c9822fc2472b4525702
MosleyDev <theawesomemosley@gmail.com>
2022-09-18 20:26:36 +0300

    butonlar

--- MagaraJam5/Assets/Scripts/ClickyButton.cs
+++ MagaraJam5/Assets/Scripts/ClickyButton.cs
@@ -5,21 +5,22 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(AudioSource))]
-public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
     [SerializeField] private Image img;
     [SerializeField] private Sprite _default, _pressed;
-    [SerializeField] private AudioClip _compressClip, _uncompressClip;
+    [SerializeField] private AudioClip _clip;
     [SerializeField] private AudioSource audioSource;
     public void OnPointerDown(PointerEventData eventData)
     {
         img.sprite = _pressed;
-        audioSource.PlayOneShot(_compressClip);
     }
     public void OnPointerUp(PointerEventData eventData)
     {
-        img.sprite = default;
-        audioSource.PlayOneShot(_uncompressClip);
+        img.sprite = _default;
     }
-
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(_clip);
+    }
 }
